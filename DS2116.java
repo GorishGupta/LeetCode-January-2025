7035c47f4f517415b99b6682976945591c089a48
Gorish Gupta <gorishgupta50@gmail.com>
2025-01-12 20:24:37 +0530

    Create DS2116.java

---
+++ DS2116.java
@@ -0,0 +1,40 @@
+public class DS2116 {
+    class Solution {
+        public boolean canBeValid(String s, String locked) {
+            int n = s.length();
+            
+            if (n % 2 != 0) {
+                return false;
+            }
+            
+            int balance = 0;
+            // Left to right pass
+            for (int i = 0; i < n; i++) {
+                if (s.charAt(i) == '(' || locked.charAt(i) == '0') {
+                    balance++;
+                } else {
+                    balance--;
+                }
+                if (balance < 0) return false;
+            }
+            
+            balance = 0;
+            // Right to left pass
+            for (int i = n - 1; i >= 0; i--) {
+                if (s.charAt(i) == ')' || locked.charAt(i) == '0') {
+                    balance++;
+                } else {
+                    balance--;
+                }
+                if (balance < 0) return false;
+            }
+            
+            return true;
+        }
+    public static void main(String[] args) {
+        String s = "(()))";
+        String locked = "01000";
+        Solution sol = new DS2116().new Solution();
+        System.out.println(sol.canBeValid(s, locked));
+    }
+}
