dd09940cf90c667f470b866f8057c92a40436aef
Gorish Gupta <gorishgupta50@gmail.com>
2025-01-18 22:40:18 +0530

    Update DS1368.java

--- DS1368.java
+++ DS1368.java
@@ -45,5 +45,6 @@ public static void main(String[] args) {
     DS1368.Solution s = d.new Solution();
     int[][] grid = {{1,1,1,1},{2,2,2,2},{1,1,1,1},{2,2,2,2}};
     System.out.println(s.minCost(grid));
+    System.out.println();
 }
 }
