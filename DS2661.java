09847b665f1ab788892afec5b4bbe04c94cc926f
Gorish Gupta <gorishgupta50@gmail.com>
2025-01-20 18:58:00 +0530

    Create DS2661.java

---
+++ DS2661.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DS2661 {
+    class Solution {
+    public int firstCompleteIndex(int[] arr, int[][] mat) {
+        int rows = mat.length, cols = mat[0].length;
+        Map<Integer, int[]> positionMap = new HashMap<>();
+        int[] rowCount = new int[rows];
+        int[] colCount = new int[cols];
+        Arrays.fill(rowCount, cols);
+        Arrays.fill(colCount, rows);
+        for (int r = 0; r < rows; ++r) {
+            for (int c = 0; c < cols; ++c) {
+                positionMap.put(mat[r][c], new int[]{r, c});
+            }
+        }
+        for (int idx = 0; idx < arr.length; ++idx) {
+            int[] pos = positionMap.get(arr[idx]);
+            if (--rowCount[pos[0]] == 0 || --colCount[pos[1]] == 0) {
+                return idx;
+            }
+        }
+        return -1;
+    }
+}
+public static void main(String[] args) {
+    DS2661 d = new DS2661();
+    DS2661.Solution s = d.new Solution();
+    int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int[][] mat = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    System.out.println(s.firstCompleteIndex(arr, mat));
+}
+}
