951adad320274f1045a5b78aa8cae5dc038dcff0
Gorish Gupta <gorishgupta50@gmail.com>
2025-01-21 22:13:52 +0530

    Update DS2017.java

--- DS2017.java
+++ DS2017.java
@@ -2,7 +2,6 @@ public class DS2017 {
     class Solution {
 
         public long gridGame(int[][] grid) {
-            // Calculate the sum of all the elements for the first row
             long firstRowSum = 0;
             for (int num : grid[0]) {
                 firstRowSum += num;
@@ -11,8 +10,6 @@ public class DS2017 {
             long minimumSum = Long.MAX_VALUE;
             for (int turnIndex = 0; turnIndex < grid[0].length; ++turnIndex) {
                 firstRowSum -= grid[0][turnIndex];
-                // Find the minimum maximum value out of firstRowSum and
-                // secondRowSum.
                 minimumSum = Math.min(
                     minimumSum,
                     Math.max(firstRowSum, secondRowSum)
@@ -23,7 +20,6 @@ public class DS2017 {
         }
     }
     public static void main(String[] args) {
-        
         DS2017 d = new DS2017();
         DS2017.Solution s = d.new Solution();
         int[][] grid = {
